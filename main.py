f389352bd2bd3d9afec2b940fbb87cb803249a9f
memristor <julio.lopezb@sansano.usm.cl>
2024-06-13 12:32:39 -0400

    changed batchsize for training

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
     # Cargar Train parameters
     train_params = get_training_params(model_name)
-    add_extra_training_params(train_params, batch=0.7, single_cls=True, plots=True, exist_ok=True, model=model_path)
+    add_extra_training_params(train_params, batch=16, single_cls=True, plots=True, exist_ok=True, model=model_path)
 
     # Entrenar
     for key, value in train_params.items():
